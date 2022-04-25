62bad4bed1b6576ddcdc4b6e392cf6b11e89ddfd
VolkieKebab <volkan.acar@hotmail.com>
2022-04-25 22:05:20 +0300

    Hesabım sekmesi eklendi.

---
+++ App4/App4/Views/Uyegirisekrani.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App4.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Uyegirisekrani : ContentPage
+    {
+        public Uyegirisekrani()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "admin" && txtPassword.Text == "admin123")
+            {
+                Navigation.PushAsync(new AboutPage());
+            }
+            else
+            {
+                DisplayAlert("Uyarı!", "E-Posta adresi veya şifreniz hatalı.", "Tamam");
+            }
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Kayitekrani());
+        }
+
+        private void TapGestureRecognizer_Tapped_1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Sifremiunuttumekrani());
+        }
+
+    }
+}
